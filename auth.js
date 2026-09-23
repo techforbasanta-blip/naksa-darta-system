a7b4a6b21db22be5f4bf80e71e8f097e1af733f0
techforbasanta-blip <techforbasanta@gmail.com>
2026-09-24 00:58:19 +0545

    Refactor authentication functions in auth.js

--- auth.js
+++ auth.js
@@ -33,7 +33,7 @@ async function handleStaffLogin(event) {
     }
 
     try {
-        const res = await fetch('/api/auth/login', {
+        const res = await fetch('/api/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ login_id, password })
